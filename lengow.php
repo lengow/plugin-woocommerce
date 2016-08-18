2b17815344701d0f98989729734dc1c46bf300b8
Sarah Touati <sarah.touati@lengow.com>
2016-08-18 10:56:04 +0200

    [ADD]Plugin folders

---
+++ lengow.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* Plugin Name: Lengow for Woocommerce 2.x
+* Plugin URI: http://www.lengow.com
+* Description: Export your catalog and synchronize your stock
+* Version: 2.0.0
+* Author: Lengow
+* Author URI: http://www.lengow.com
+* Requires at least: 3.5
+* Tested up to: 4.6
+*
+* Text Domain: lengow
+* Domain Path: /languages
+*
+* @package Lengow
+* @author Lengow
+*/
