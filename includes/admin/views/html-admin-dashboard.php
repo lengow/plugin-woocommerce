aa92667df53faa01ff287ebe865457b2b276f3e4
benjamin <benjamin.le-neve@lengow>
2016-08-23 17:59:06 +0200

    Add new translation system

--- includes/admin/views/html-admin-dashboard.php
+++ includes/admin/views/html-admin-dashboard.php
@@ -5,6 +5,8 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-
+$locale = new Lengow_Translation();
 ?>
-<h2>Hello world!</h2>
+
+<h2><?php echo $locale->t('dashboard.hello_world'); ?></h2>
+<b><?php echo $locale->t('dashboard.new_friend', array('nb' => 100)); ?></b>
