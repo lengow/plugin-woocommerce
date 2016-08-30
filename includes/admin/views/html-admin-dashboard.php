8c6a077bdc2ac21eeaae33a6e65abc522084c657
benjamin <benjamin.le-neve@lengow>
2016-08-30 14:29:18 +0200

    Add translation for extension page

--- includes/admin/views/html-admin-dashboard.php
+++ includes/admin/views/html-admin-dashboard.php
@@ -5,8 +5,4 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-$locale = new Lengow_Translation();
 ?>
-
-<h2><?php echo $locale->t('dashboard.hello_world'); ?></h2>
-<b><?php echo $locale->t('dashboard.new_friend', array('nb' => 100)); ?></b>
