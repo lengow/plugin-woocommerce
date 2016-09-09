96b28559f3932f2348c2916ac88d548bf35b2dc6
Sarah Touati <sarah.touati@lengow.com>
2016-09-09 17:50:30 +0200

    [ADD] Product page

--- includes/admin/class-lengow-admin-products.php
+++ includes/admin/class-lengow-admin-products.php
@@ -18,26 +18,17 @@ if( ! class_exists( 'WP_List_Table' ) ) {
  * Lengow_Admin_Products Class.
  */
 class Lengow_Admin_Products extends WP_List_Table {
-    public $example_data = array(
-        array('ID' => 1,'booktitle' => 'Quarter Share', 'author' => 'Nathan Lowell',
-            'isbn' => '978-0982514542'),
-        array('ID' => 2, 'booktitle' => '7th Son: Descent','author' => 'J. C. Hutchins',
-            'isbn' => '0312384378'),
-        array('ID' => 3, 'booktitle' => 'Shadowmagic', 'author' => 'John Lenahan',
-            'isbn' => '978-1905548927'),
-        array('ID' => 4, 'booktitle' => 'The Crown Conspiracy', 'author' => 'Michael J. Sullivan',
-            'isbn' => '978-0979621130'),
-        array('ID' => 5, 'booktitle'     => 'Max Quick: The Pocket and the Pendant', 'author'    => 'Mark Jeffrey',
-            'isbn' => '978-0061988929'),
-        array('ID' => 6, 'booktitle' => 'Jack Wakes Up: A Novel', 'author' => 'Seth Harwood',
-            'isbn' => '978-0307454355')
-    );
+    public $data;
+
 
     function get_columns(){
         $columns = array(
-            'booktitle' => 'Title',
-            'author'    => 'Author',
-            'isbn'      => 'ISBN'
+            'cb'            => '<input type="checkbox" />',
+            'ID'            => 'ID',
+            'post_title'    => 'Name',
+            '_sku'          => 'Sku',
+            '_stock_status' => 'Stock',
+            '_price'        => 'Price',
         );
         return $columns;
     }
@@ -45,16 +36,20 @@ class Lengow_Admin_Products extends WP_List_Table {
     function prepare_items() {
         $columns = $this->get_columns();
         $hidden = array();
-        $sortable = array();
+        $sortable = $this->get_sortable_columns();
         $this->_column_headers = array($columns, $hidden, $sortable);
-        $this->items = $this->example_data;
+        $this->data = $this->get_products();
+        usort( $this->data, array( &$this, 'usort_reorder' ));
+        $this->items =  $this->data;
     }
 
     function column_default( $item, $column_name ) {
         switch( $column_name ) {
-            case 'booktitle':
-            case 'author':
-            case 'isbn':
+            case 'ID':
+            case 'post_title':
+            case '_sku':
+            case '_stock_status':
+            case '_price':
                 return $item[ $column_name ];
             default:
 
@@ -63,20 +58,91 @@ class Lengow_Admin_Products extends WP_List_Table {
 
 
     static function my_render_list_page(){
-        $myListTable = new Lengow_Admin_Products();
-        echo '<div class="wrap"><h2>My List Table Test</h2>';
-        $myListTable->prepare_items();
-        $myListTable->display();
-        echo '</div>';
+        $lengow_table = new Lengow_Admin_Products();
+        $lengow_table->prepare_items();
+        $lengow_table->display();
     }
 
     function get_sortable_columns() {
         $sortable_columns = array(
-            'booktitle'  => array('booktitle',true),
-            'author' => array('author',false),
-            'isbn'   => array('isbn',false)
+            'ID'  => array('ID',true),
+            'post_title' => array('post_title',false),
+            '_sku' => array('_sku',false),
+            '_stock_status' => array('_stock_status',false),
+            '_price' => array('_price',false),
         );
         return $sortable_columns;
+    }
+
+    function usort_reorder( $a, $b ) {
+        // If no sort, default to title
+        $order_by = ( ! empty( $_GET['orderby'] ) ) ? $_GET['orderby'] : 'ID';
+        // If no order, default to asc
+        $order = ( ! empty($_GET['order'] ) ) ? $_GET['order'] : 'asc';
+        // Determine sort order
+        $result = strcmp( $a[$order_by], $b[$order_by] );
+        // Send final sort direction to usort
+        return ( $order === 'asc' ) ? $result : -$result;
+    }
+
+    function column_ID($item) {
+        $actions = array(
+            'edit'      => sprintf('<a href="?page=%s&action=%s&book=%s">Edit</a>',$_REQUEST['page'],'edit',$item['ID']),
+        );
+
+        return sprintf('%1$s %2$s', $item['ID'], $this->row_actions($actions) );
+    }
+
+    function get_bulk_actions() {
+        $actions = array(
+            'delete'    => 'Delete'
+        );
+        return $actions;
+    }
+
+    function column_cb($item) {
+        return sprintf(
+            '<input type="checkbox" name="product[]" value="%s" />', $item['ID']
+        );
+    }
+
+    function get_products(){
+        $results = array();
+        $keys =  array(
+            'ID',
+            'post_title',
+            'categories',
+            '_stock_status',
+            '_downloadable',
+            '_virtual',
+            '_sku',
+            '_price'
+        );
+
+        $products = get_posts(
+                        array(
+                            'numberposts' => -1,
+                            'post_type' => 'product'
+                        ));
+
+        foreach ($keys as $key) {
+            foreach ($products as $product) {
+                switch ($key) {
+                    case 'ID' : $products_data = $product->ID;
+                        break;
+                    case 'post_title' : $products_data = $product->post_title;
+                        break;
+//                    case 'categories' :
+//                        break;
+//                    case '_downloadable' :
+//                    case '_virtual' :
+                        break;
+                    default : $products_data = get_post_meta($product->ID, $key, true);
+                }
+                $results[$product->ID][$key] = $products_data;
+            }
+        }
+        return $results;
     }
 
     /**
