1432430e3b82b556ee63ef991a84db9c4da3f7a6
Alexandre Rabottin <alexandre.rabottin@lengow.com>
2016-10-12 17:11:43 +0200

    FIX path require_once

--- toolbox/views/head.php
+++ toolbox/views/head.php
@@ -1,16 +1,16 @@
 <?php
 define( 'WP_USE_THEMES', false );
-require_once( $_SERVER['DOCUMENT_ROOT'].'/wp-load.php' );
-include_once( $_SERVER['DOCUMENT_ROOT'].'/wp-content/plugins/lengow-woocommerce/lengow.php');
-include_once( $_SERVER['DOCUMENT_ROOT'].'/wp-content/plugins/lengow-woocommerce/includes/class-lengow-configuration.php' );
-include_once( $_SERVER['DOCUMENT_ROOT'].'/wp-content/plugins/lengow-woocommerce/includes/class-lengow-translation.php' );
-include_once( $_SERVER['DOCUMENT_ROOT'].'/wp-content/plugins/lengow-woocommerce/includes/class-lengow-feed.php' );
-include_once( $_SERVER['DOCUMENT_ROOT'].'/wp-content/plugins/lengow-woocommerce/includes/class-lengow-export.php' );
-include_once( $_SERVER['DOCUMENT_ROOT'].'/wp-content/plugins/lengow-woocommerce/includes/class-lengow-check.php' );
-include_once( $_SERVER['DOCUMENT_ROOT'].'/wp-content/plugins/lengow-woocommerce/includes/class-lengow-main.php' );
-include_once( $_SERVER['DOCUMENT_ROOT'].'/wp-content/plugins/lengow-woocommerce/includes/class-lengow-file.php' );
-include_once( $_SERVER['DOCUMENT_ROOT'].'/wp-content/plugins/lengow-woocommerce/includes/class-lengow-log.php' );
-include_once( $_SERVER['DOCUMENT_ROOT'].'/wp-content/plugins/lengow-woocommerce/includes/class-lengow-import.php' );
+require( $_SERVER['DOCUMENT_ROOT'] . '/wp-load.php' );
+require_once( '../lengow.php');
+require_once( '../includes/class-lengow-configuration.php' );
+require_once( '../includes/class-lengow-translation.php' );
+require_once( '../includes/class-lengow-feed.php' );
+require_once( '../includes/class-lengow-export.php' );
+require_once( '../includes/class-lengow-check.php' );
+require_once( '../includes/class-lengow-main.php' );
+require_once( '../includes/class-lengow-file.php' );
+require_once( '../includes/class-lengow-log.php' );
+require_once( '../includes/class-lengow-import.php' );
 $locale = new Lengow_Translation();
 $check = new Lengow_Check();
 ?>
