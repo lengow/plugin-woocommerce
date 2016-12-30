13ed6a119c2868f9a3e420c4d233e94a20830022
Benjamin Le Nevé <benjamin.le-neve@lengow.com>
2016-12-30 11:47:53 +0100

    Change copyright date

--- includes/admin/views/html-admin-footer.php
+++ includes/admin/views/html-admin-footer.php
@@ -14,7 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) {
                 <a href="<?php echo admin_url('admin.php?page=lengow&tab=lengow_admin_legals'); ?>"
                    class="sub-link" title="Legal"><?php echo $locale->t( 'footer.legals' ) ?></a>
                 | <?php echo $locale->t( 'footer.plugin_lengow' ) ?> -
-                copyright © 2016  <a href="http://www.lengow.com" target="_blank" class="sub-link" title="Lengow.com">Lengow</a>
+                copyright © 2017  <a href="http://www.lengow.com" target="_blank" class="sub-link" title="Lengow.com">Lengow</a>
             </p>
         </div>
     </div>
