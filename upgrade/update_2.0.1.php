68715d65eea46fbe6dbfa59d899bf4e1cd02d354
Benjamin Le Nevé <benjamin.le-neve@lengow.com>
2017-12-22 16:04:29 +0100

    Hotfix: Change css style for lengow order status

---
+++ upgrade/update_2.0.1.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Update 2.0.1
+ *
+ * Copyright 2017 Lengow SAS
+ *
+ * NOTICE OF LICENSE
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * at your option) any later version.
+ *
+ * It is available through the world-wide-web at this URL:
+ * https://www.gnu.org/licenses/old-licenses/gpl-2.0
+ *
+ * @category    Lengow
+ * @package     lengow-woocommerce
+ * @subpackage  upgrade
+ * @author      Team module <team-module@lengow.com>
+ * @copyright   2017 Lengow SAS
+ */
+
+if ( ! defined( 'ABSPATH' ) || ! Lengow_Install::is_installation_in_progress() ) {
+	exit;
+}
