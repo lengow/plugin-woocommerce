20d37f8e3c65f2d4ce0bef8b04a38aed7532e81e
Benjamin Le Nevé <benjamin.le-neve@lengow.com>
2019-07-19 15:47:26 +0200

    Fix css for footer

--- includes/admin/views/html-admin-footer.php
+++ includes/admin/views/html-admin-footer.php
@@ -7,10 +7,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 ?>
-<div class="lgw-container lgw-footer-vold clear">
+<div class="lgw-container lgw-footer clear">
 	<div class="lgw-content-section text-center">
 		<div id="lgw-footer">
-			<p class="pull-right">
+			<p>
 				<a href="<?php echo admin_url('admin.php?page=lengow&tab=lengow_admin_legals'); ?>"
 				   class="sub-link" title="Legal"><?php echo $locale->t( 'footer.legals' ); ?></a>
 				| <?php echo $locale->t( 'footer.plugin_lengow' ) ?> - v.<?php echo LENGOW_VERSION; ?>
