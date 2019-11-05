7f5fad30e907aea5faf5718bd3ed1d1e9a29ded2
Benjamin Le Nevé <benjamin.le-neve@lengow.com>
2019-11-05 10:18:37 +0100

    Ad compatibility with old versions

--- includes/frontend/class-lengow-tracker.php
+++ includes/frontend/class-lengow-tracker.php
@@ -37,39 +37,89 @@ class Lengow_Tracker {
 	 */
 	public static function html_display( $order ) {
 		try {
-			$account_id   = Lengow_Configuration::get( 'lengow_account_id' );
-			$tracking_id  = Lengow_Configuration::get( 'lengow_tracking_id' );
-			$product_cart = array();
-			foreach ( $order->get_items() as $item ) {
-				$product = Lengow_Product::get_product( $item->get_product_id() );
-				if ( 'sku' === $tracking_id ) {
-					$product_id = $product->get_sku();
-				} else {
-					$product_id = 'variation' === Lengow_Product::get_product_type( $product )
-						? $item->get_product_id() . '_' . $item->get_variation_id()
-						: $item->get_product_id();
-				}
-				$product_cart[] = array(
-					'product_id' => $product_id,
-					'price'      => (float) $item->get_total() + (float) $item->get_total_tax(),
-					'quantity'   => (int) $item->get_quantity(),
-				);
-			}
-			$tracker = array(
-				'account_id'     => $account_id,
-				'order_ref'      => Lengow_Order::get_order_id( $order ),
-				'amount'         => (float) $order->get_total(),
-				'currency'       => $order->get_currency(),
-				'payment_method' => $order->get_payment_method(),
-				'cart'           => htmlspecialchars( json_encode( $product_cart ) ),
-				'cart_number'    => 0,
-				'newbiz'         => 1,
-				'valid'          => 1,
-			);
-
+			$account_id     = Lengow_Configuration::get( 'lengow_account_id' );
+			$order_ref      = Lengow_Order::get_order_id( $order );
+			$amount         = (float) $order->get_total();
+			$currency       = self::_get_currency( $order );
+			$payment_method = self::_get_payment_method( $order );
+			$cart           = htmlspecialchars( json_encode( self::_get_product_cart( $order->get_items() ) ) );
+			$cart_number    = 0;
+			$newbiz         = 1;
+			$valid          = 1;
 			include_once( 'views/tracker/html-tracker.php' );
 		} catch ( Exception $e ) {
 			echo '';
 		}
+	}
+
+	/**
+	 * Get product cart with id, price and quantity foreach product.
+	 *
+	 * @param array $items WooCommerce order items
+	 *
+	 * @return array
+	 */
+	private static function _get_product_cart( $items ) {
+		$tracking_id  = Lengow_Configuration::get( 'lengow_tracking_id' );
+		$product_cart = array();
+		foreach ( $items as $item ) {
+			if ( Lengow_Main::compare_version( '3.0' ) ) {
+				$product  = Lengow_Product::get_product( $item->get_product_id() );
+				$price    = wc_get_price_including_tax( $product );
+				$quantity = (int) $item->get_quantity();
+			} else {
+				$product  = Lengow_Product::get_product( $item['product_id'] );
+				$price    = (float) $product->get_price_including_tax();
+				$quantity = (int) $item['qty'];
+			}
+			if ( 'sku' === $tracking_id ) {
+				$product_id = $product->get_sku();
+			} else {
+				$product_id = 'variation' === Lengow_Product::get_product_type( $product )
+					? Lengow_Product::get_product_id( $product ) . '_' . Lengow_Product::get_variation_id( $product )
+					: Lengow_Product::get_product_id( $product );
+			}
+			$product_cart[] = array(
+				'product_id' => $product_id,
+				'price'      => $price,
+				'quantity'   => $quantity,
+			);
+		}
+
+		return $product_cart;
+	}
+
+	/**
+	 * Get currency for old WooCommerce versions.
+	 *
+	 * @param WC_Order $order WooCommerce order instance
+	 *
+	 * @return string
+	 */
+	private static function _get_currency( $order ) {
+		if ( Lengow_Main::compare_version( '3.0' ) ) {
+			$currency = $order->get_currency();
+		} else {
+			$currency = get_post_meta( Lengow_Order::get_order_id( $order ), '_order_currency', true );
+		}
+
+		return $currency;
+	}
+
+	/**
+	 * Get payment method for old WooCommerce versions.
+	 *
+	 * @param WC_Order $order WooCommerce order instance
+	 *
+	 * @return string
+	 */
+	private static function _get_payment_method( $order ) {
+		if ( Lengow_Main::compare_version( '3.0' ) ) {
+			$payment_method = $order->get_payment_method();
+		} else {
+			$payment_method = get_post_meta( Lengow_Order::get_order_id( $order ), '_payment_method', true );
+		}
+
+		return $payment_method;
 	}
 }
--- includes/frontend/views/tracker/html-tracker.php
+++ includes/frontend/views/tracker/html-tracker.php
@@ -8,6 +8,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 ?>
 <!-- Tag_Lengow -->
-<img src="https://trk.lgw.io/lead?account_id=><?php echo $tracker['account_id']; ?>&order_ref=<?php echo $tracker['order_ref']; ?>&amount=<?php echo $tracker['amount']; ?>&currency=<?php echo $tracker['currency']; ?>&payment_method=<?php echo $tracker['payment_method']; ?>&cart=<?php echo $tracker['cart']; ?>&cart_number=<?php echo $tracker['cart_number']; ?>&newbiz=<?php echo $tracker['newbiz']; ?>" alt="" style="width: 1px; height: 1px; border: none;" />
-<img src="https://trk.lgw.io/validation?account_id=<?php echo $tracker['account_id']; ?>&order_ref=<?php echo $tracker['order_ref']; ?>&payment_method=<?php echo $tracker['payment_method']; ?>&valid=<?php echo $tracker['valid']; ?>" alt="" style="width: 1px; height: 1px; border: none;" />
+<img src="https://trk.lgw.io/lead?account_id=><?php echo $account_id; ?>&order_ref=<?php echo $order_ref; ?>&amount=<?php echo $amount; ?>&currency=<?php echo $currency; ?>&payment_method=<?php echo $payment_method; ?>&cart=<?php echo $cart; ?>&cart_number=<?php echo $cart_number; ?>&newbiz=<?php echo $newbiz; ?>" alt="" style="width: 1px; height: 1px; border: none;" />
+<img src="https://trk.lgw.io/validation?account_id=<?php echo $account_id; ?>&order_ref=<?php echo $order_ref; ?>&payment_method=<?php echo $payment_method; ?>&valid=<?php echo $valid; ?>" alt="" style="width: 1px; height: 1px; border: none;" />
 <!-- /Tag_Lengow -->
