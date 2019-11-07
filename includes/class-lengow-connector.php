68e9ddb63ae53d32b3915d34d2a1b8e3d7810bbb
Benjamin Le Nevé <benjamin.le-neve@lengow.com>
2019-11-07 17:21:11 +0100

    Change const LENGOW_API_URL for recette

--- includes/class-lengow-connector.php
+++ includes/class-lengow-connector.php
@@ -34,9 +34,9 @@ class Lengow_Connector {
 	/**
 	 * @var string url of the API Lengow.
 	 */
-	const LENGOW_API_URL = 'https://api.lengow.io';
+	//const LENGOW_API_URL = 'https://api.lengow.io';
 	// const LENGOW_API_URL = 'https://api.lengow.net';
-	// const LENGOW_API_URL = 'http://api.lengow.rec';
+	const LENGOW_API_URL = 'http://api.lengow.rec';
 	// const LENGOW_API_URL = 'http://10.100.1.82:8081';
 
 	/**
