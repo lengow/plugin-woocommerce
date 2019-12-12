84d35f6f6bd1895fb630a1db3412cb9361e293f3
Benjamin Le Nevé <benjamin.le-neve@lengow.com>
2019-12-12 15:03:55 +0100

    Feature: Update of the GNU General Public License in version 3

--- includes/admin/class-lengow-admin-help.php
+++ includes/admin/class-lengow-admin-help.php
@@ -8,11 +8,11 @@
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation; either version 3 of the License, or
  * at your option) any later version.
  * 
  * It is available through the world-wide-web at this URL:
- * https://www.gnu.org/licenses/old-licenses/gpl-2.0
+ * https://www.gnu.org/licenses/gpl-3.0
  *
  * @category   	Lengow
  * @package    	lengow-woocommerce
