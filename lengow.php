868b1a6acaf906c02953389aaae5a8c414eb332b
Benjamin Le Nevé <benjamin.le-neve@lengow.com>
2019-12-12 10:40:20 +0100

    Declare new version 2.2.1

--- lengow.php
+++ lengow.php
@@ -26,7 +26,7 @@
  * Plugin Name: Lengow for WooCommerce
  * Plugin URI: http://www.lengow.com
  * Description: Lengow allows you to easily export your product catalogue from your WooCommerce store and sell on Amazon, Cdiscount, Google Shopping, Criteo, LeGuide.com, Ebay, Bing,... Choose from our 1,800 available marketing channels!
- * Version: 2.2.0
+ * Version: 2.2.1
  * Author: Lengow
  * Author URI: http://www.lengow.com
  * Requires at least: 3.5
@@ -59,7 +59,7 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
 		/**
 		 * @var string current version of plugin.
 		 */
-		public $version = '2.2.0';
+		public $version = '2.2.1';
 
 		/**
 		 * @var string plugin name.
